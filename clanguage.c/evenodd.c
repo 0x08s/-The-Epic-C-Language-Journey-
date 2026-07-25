b981c410d6cb65774ff69533a3ed1574ea346bd0
sawera <241689067+echomesawera@users.noreply.github.com>
2026-07-25 20:14:31 +0500

    testing

--- clanguage.c/evenodd.c
+++ clanguage.c/evenodd.c
@@ -1,18 +1,10 @@
 #include<stdio.h>
+
+
 //function prototype
 void printmessage(int a);
 
-int main(){
-int a;
-printf("enter your number");
-scanf("%d",&a);
-//calling the function
-printmessage(a);
-return 0;
-}
-
 //function definition
-
 void printmessage(int a){
 if(a%2==0){
     printf("print the even number ");
