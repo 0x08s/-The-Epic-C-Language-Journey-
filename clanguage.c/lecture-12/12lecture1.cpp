2c43f07e599b5b08d7ed93f45124ac3c9e928551
sawera <241689067+echomesawera@users.noreply.github.com>
2026-08-01 22:35:45 +0500

    code

--- clanguage.c/lecture-12/12lecture1.cpp
+++ clanguage.c/lecture-12/12lecture1.cpp
@@ -15,7 +15,7 @@ int main(){
     cout<<"\nMatrix (2 rows x 3 columns):\n";
     for(int i=0; i<2; i++){
         for(int j=0; j<3; j++)
-            cout<<matrix[i][j]<<"\t";
+            cout<<matrix[i][j]<<;
         cout<<"\n";
     }
 
