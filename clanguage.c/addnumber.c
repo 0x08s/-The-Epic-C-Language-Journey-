cf9ba5d457812924f66a4bde6f2792e51fb68285
sawera <241689067+echomesawera@users.noreply.github.com>
2026-08-03 18:24:21 +0500

    code

--- clanguage.c/addnumber.c
+++ clanguage.c/addnumber.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
-int main (){ 
+int main (){
 int marks[10] = {10,20,30,40,50,60,70,80,90,100};
 
 for(int i = 0;i<=10;i++){
-  
+
 printf("%d\n",i);
 
+// condition
 
 if(marks[i]<35){
 
