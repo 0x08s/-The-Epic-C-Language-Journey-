f812092216674efc83c648a9ae6782c65528a779
sawera <241689067+echomesawera@users.noreply.github.com>
2026-08-19 16:41:11 +0500

    code

--- clanguage.c/lecture-13/13lecture2.cpp
+++ clanguage.c/lecture-13/13lecture2.cpp
@@ -6,7 +6,7 @@ int main(){
     int empID[5];
     int minSalary;
     int unluckyEmp;
-    
+
     // Input employee IDs and salaries
     cout<<"Enter Employee ID and Salary for 5 Employees:\n";
     for(int i=0; i<5; i++){
@@ -15,33 +15,34 @@ int main(){
         cout<<"Employee "<<i+1<<" Salary: ";
         cin>>salary[i];
     }
-    
+
     // Display all employees
 
     cout<<"\nEmployee Record\n";
-    
+
     cout<<"ID\tSalary\n";
 
     for(int i=0; i<5; i++)
-        cout<<empID[i]<<"\t"<<salary[i]<<"\n";
     
+        cout<<empID[i]<<"\t"<<salary[i]<<"\n";
+
     // Find minimum salary (unlucky employee)
 
     minSalary = salary[0];
     unluckyEmp = empID[0];
-    
+
     for(int i=1; i<5; i++){
         if(salary[i] < minSalary){
             minSalary  = salary[i];
             unluckyEmp = empID[i];
         }
     }
-    
+
     // Display result
 
     cout<<"\n Result \n";
     cout<<"Unlucky Employee ID : "<<unluckyEmp<<"\n";
     cout<<"Lowest Salary       : "<<minSalary<<"\n";
-    
+
     return 0;
 }
