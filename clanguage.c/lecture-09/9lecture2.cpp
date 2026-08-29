d070f8ce2e3b0b87a9a743c0c144305dc65308dd
sawera <241689067+echomesawera@users.noreply.github.com>
2026-08-29 19:00:11 +0500

    code

--- clanguage.c/lecture-09/9lecture2.cpp
+++ clanguage.c/lecture-09/9lecture2.cpp
@@ -11,10 +11,11 @@ int raiseToPow(int base, int exponent) {
 }
 
 int main() {
+
     int num, power, answer;
 
     // User se input lena
-    
+
     cout << "Number enter karein: ";
     cin >> num;
     cout << "Power enter karein: ";
